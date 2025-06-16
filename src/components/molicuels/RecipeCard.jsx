0c0a4ed63c6547c132818c5af6b044413f13612b
MeetKhetani2003 <meetkhetani1111@gmail.com>
2025-06-16 16:19:38 +0530

    added

--- src/components/molicuels/RecipeCard.jsx
+++ src/components/molicuels/RecipeCard.jsx
@@ -1,16 +1,18 @@
-const RecipeCard = ({ img, heading, dec }) => {
+const RecipeCard = ({ videoId, heading, dec }) => {
   return (
     <div>
       <div>
-        <img
-          className="w-full max-h-64 object-cover rounded-2xl shadow-xl shadow-gray-100"
-          src={img}
-          alt="recipe"
-        />
-        <p className="mt-7 uppercase font-serif text-xl  text-center ">
+        <iframe
+          className='w-full min-h-64 object-cover rounded-2xl shadow-xl shadow-gray-100'
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title={heading}
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        ></iframe>
+        <p className='mt-7 uppercase font-serif text-xl text-center'>
           {heading}
         </p>
-        <p className="text-gray-600 text-center font-montserrat">{dec}</p>
+        <p className='text-gray-600 text-center font-montserrat'>{dec}</p>
       </div>
     </div>
   );
--- src/components/organisms/Home/RecipeContainer.jsx
+++ src/components/organisms/Home/RecipeContainer.jsx
@@ -1,43 +1,44 @@
 import RecipeCard from '@/components/molicuels/RecipeCard';
-import { assets } from '@/utils/AssetImport';
+
+// Define an array of video data with YouTube links and other details
+const recipes = [
+  {
+    videoId: 'yW-GF7Tpf10',
+    heading: 'Sweet orange yellow from Australia',
+    description: '2021-08-27  No Comments',
+  },
+  {
+    videoId: 'TxvqhxzTp4U',
+    heading: 'Sweet orange yellow from Australia',
+    description: '2021-08-27  No Comments',
+  },
+  {
+    videoId: 'H6sy8LKAzAk',
+    heading: 'Sweet orange yellow from Australia',
+    description: '2021-08-27  No Comments',
+  },
+  {
+    videoId: 'Dlv-FIr5l18',
+    heading: 'Sweet orange yellow from Australia',
+    description: '2021-08-27  No Comments',
+  },
+];
 
 const RecipeContainer = () => {
   return (
-    <div className="mt-20 mx-8 lg:max-w-7xl lg:mx-auto ">
-      <h1 className="text-5xl uppercase font-semibold font-serif text-center ">
+    <div className='mt-20 mx-8 lg:max-w-7xl lg:mx-auto'>
+      <h1 className='text-5xl uppercase font-semibold font-serif text-center'>
         recipes & tips
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-20 mt-24">
-        <RecipeCard
-          img={assets.re1}
-          heading={'Sweet orange yellow from Australia'}
-          dec={'2021-08-27  No Comments'}
-        />
-        <RecipeCard
-          img={assets.re1}
-          heading={'Sweet orange yellow from Australia'}
-          dec={'2021-08-27  No Comments'}
-        />
-        <RecipeCard
-          img={assets.re1}
-          heading={'Sweet orange yellow from Australia'}
-          dec={'2021-08-27  No Comments'}
-        />
-        <RecipeCard
-          img={assets.re1}
-          heading={'Sweet orange yellow from Australia'}
-          dec={'2021-08-27  No Comments'}
-        />
-        <RecipeCard
-          img={assets.re1}
-          heading={'Sweet orange yellow from Australia'}
-          dec={'2021-08-27  No Comments'}
-        />
-        <RecipeCard
-          img={assets.re1}
-          heading={'Sweet orange yellow from Australia'}
-          dec={'2021-08-27  No Comments'}
-        />
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-20 mt-24'>
+        {recipes.map((recipe, index) => (
+          <RecipeCard
+            key={index}
+            videoId={recipe.videoId}
+            heading={recipe.heading}
+            dec={recipe.description}
+          />
+        ))}
       </div>
     </div>
   );
