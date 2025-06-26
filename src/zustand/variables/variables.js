86bad0cfcdf9a03f1668f9aa461dced934ef0ded
Groovy2024 <groovycaferajkot@gmail.com>
2025-06-26 10:20:59 +0530

    Update variables.js

--- src/zustand/variables/variables.js
+++ src/zustand/variables/variables.js
@@ -1,5 +1,5 @@
 export const variables = {
   // backend_url: 'http://localhost:3000/api/v1',
-  backend_url: 'https://groovy-frozen.onrender.com/api/v1',
+  backend_url: 'https://groovy-frozen-server.onrender.com/api/v1',
   jwt_secret: import.meta.env.VITE_JWT_SECRET,
 };
