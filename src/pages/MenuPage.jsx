fee4a1b7ba9e5fcd0e40be1c7ec48e99ae51ff8c
MeetKhetani2003 <meetkhetani1111@gmail.com>
2025-09-08 22:10:45 +0530

    added

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -31,7 +31,7 @@ const Menu = () => {
   const [categories, setCategories] = useState(['All Categories']);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageLimit] = useState(20);
+  const [pageLimit] = useState(50);
   const { addPacketToCart } = useCart();
 
   const [filters, setFilters] = useState({
